43141348fca2448e9da3b894630258af2e9cb210
alvarado0925 <alvarado0925@gmail.com>
2025-08-27 11:09:44 -0500

    feat: integrate GraphQL with Apollo Server and NestJS
    - Added GraphQL support using Apollo Server and NestJS.
    - Created GraphQL schema and resolvers for item management.
    - Implemented create, update, find, and remove operations for items.
    - Introduced DTOs for item creation and updating.
    - Enabled global validation pipes for input validation.
    - Updated package dependencies for Apollo and GraphQL.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
+import { join } from 'path';
+
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ItemsModule } from './modules/items/items.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false, // Deshabilitar playground en producción
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    ItemsModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
+  logger.log(`🚀 Application is running on: http://localhost:3000`);
+  logger.log(`🛑 GraphQL Playground: http://localhost:3000/graphql`);
 }
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
